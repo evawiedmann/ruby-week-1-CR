81785802c8987b9ab095f3b7d132f3ebe1a7570d
Eva Wiedmann <evawiedmann@gmail.com>
2019-12-13 12:24:52 -0800

    test 2 failed

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,16 @@
-class Word
+class Anagrams
   def initialize(word)
     @word = word
+    @compare_word = compare_word
   end
 
   def compare()
-
+    compare_array = @compare_word.chars()
+    letters = @word.chars()
+    letters.each do |letter|
+      if compare_array.include?(letter)
+          puts 'These words are anagrams'
+        end
+      end
     end
   end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,4 +6,8 @@ describe ('#anagrams') do
     word = Word.new('ta','at')
     expect(word.compare()).to(eq(true))
   end
+  it("is not case sensitive") do
+    word = Word.new('Ta','at')
+    expect(word.compare()).to(eq(true))
+  end
 end
