f13fcb9ee44b8c55b035dce04e85b76dfc1dd286
Eva Wiedmann <evawiedmann@gmail.com>
2020-01-07 21:01:38 -0800

    correct logic, all specs passing

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,23 +4,30 @@ class Word
     @compare_word = compare_word
   end
 
-  def compare()
-    compare_word = @compare_word.downcase()
-    compare_word.gsub(/[^0-9a-z]/i, ‘’)
-    compare_array = @compare_word.chars()
-    letters = @word.downcase()
-    letters.gsub(/[^0-9a-z]/i, ‘’)
-    letters = @word.chars()
-    if @word.include? 'a' || 'e' || 'i' || 'o' || 'u' || 'y'
+  def anagram()
+    compare_array = @compare_word.downcase.gsub(/[^0-9a-z]/i, '').chars
+    letters = @word.downcase.gsub(/[^0-9a-z]/i, '').chars()
+    puts compare_array
+    puts letters
 
-      letters.each do |letter|
-        if compare_array.include?(letter)
-          puts 'These words are anagrams'
-        elsif compare_array.exclude?(letter)
-          puts 'These words are antigrams'
-        end
-      end
-      puts 'You need to input actual words!'
+    if !(@word.include? 'a' || 'e' || 'i' || 'o' || 'u' || 'y')
+      return 'You need to input actual words!'
     end
+    antigram = true
+    anagram = true
+    letters.each do |letter|
+      if !(compare_array.include?(letter))
+        anagram = false
+      end
+      if (compare_array.include?(letter))
+        antigram = false
+      end
+    end
+    if anagram
+      return 'These words are anagrams'
+    elsif antigram
+      return 'These words are antigrams'
+    end
+    return 'These words are not anagrams'
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,25 +1,33 @@
 require('rspec')
 require('anagrams')
 
-describe ('#anagrams') do
+describe ('#anagram') do
   it("returns if two words are anagrams") do
-    word = Word.new('ta','at')
-    expect(word.compare()).to(eq(true))
+    word = Word.new('a','a')
+    expect(word.anagram()).to(eq('These words are anagrams'))
+  end
+  it("returns if two words are not anagrams") do
+    word = Word.new('a','t')
+    expect(word.anagram()).to(eq('These words are antigrams'))
+  end
+  it("returns if two words are not anagrams") do
+    word = Word.new('at','ap')
+    expect(word.anagram()).to(eq('These words are not anagrams'))
   end
   it('is not case sensitive') do
     word = Word.new('Ta','at')
-    expect(word.compare()).to(eq(true))
+    expect(word.anagram()).to(eq('These words are anagrams'))
   end
   it('returns an error message if the input has no vowels') do
     word = Word.new('tt','at')
-    expect(word.compare().to(eq('You need to input actual words!')))
+    expect(word.anagram()).to(eq('You need to input actual words!'))
   end
   it('returns if two words are antigrams') do
     word = Word.new('at', 'by')
-    expect(word.compare()).to(eq(true))
+    expect(word.anagram()).to(eq('These words are antigrams'))
   end
   it('returns result with words or phrases and ignores spaces and punctuation') do
     word = Word.new('at', 'by')
-    expect(word.compare()).to(eq(true))
+    expect(word.anagram()).to(eq('These words are antigrams'))
   end
 end
